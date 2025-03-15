9c9098733619411945f0e3e3980d22b2c66ec4b8
Espen Hovlandsdal <espen@hovlandsdal.com>
2025-03-15 16:55:30 -0700

    chore: format with prettier

--- oneline.js
+++ oneline.js
@@ -1,5 +1,5 @@
 module.exports = (strings, ...keys) =>
- strings
-    .reduce((result, part, i) => result + part + (keys[i] || '') , '')
+  strings
+    .reduce((result, part, i) => result + part + (keys[i] || ''), '')
     .replace(/(?:\n(?:\s*))+/g, ' ')
     .trim()
